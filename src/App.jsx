f0cd30f13b183fe4c3bdbc9ed462cd63eb00fba7
ghernan8 <ghernan8@uwo.ca>
2024-02-10 17:10:05 -0500

    Updated Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -17,3 +17,4 @@ function App() {
 }
 
 export default App;
+//to deploy app you do npm run deploy
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
+import '../styles/components/Navbar.css'
 
 const Navbar = () => {
     return(
-        <div>
-            hello
+        <div className='Navbar'>
+            <div className='NavbarTitle'>
+                <h1>Gabriel Hernandez's Website</h1>
+            </div>
+                <div className='NavbarSection'>
+                    <NavLink to='/'>About me</NavLink>
+                    <NavLink to='/projects'>Projects</NavLink>
+                    <NavLink to='/resume'>Resume</NavLink>
+                </div>
         </div>
+        
     );
 }
 
