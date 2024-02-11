a6545c21242d355d436f7e746432f0a6075a4247
ghernan8 <ghernan8@uwo.ca>
2024-02-11 18:28:34 -0500

    updated homepage with api inspirational quotes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,87 @@
-
+import {React, useEffect, useState} from 'react';
+import '../styles/home/Home.css'
+import '../styles/home/PanelOne.css'
+import Gabe from '../assets/home/Gabe.png'
+import '../styles/home/PanelTwo.css'
 
 const Home = () => {
     return (
-        <div>
-            <Test/>
+        <div className='Home'>
+            <PanelOne/>
+            <PanelTwo/>
+
         </div>
     );
 }
 
-const Test = () => {
+const PanelOne = () => { 
     return(
-        <div>
-            <h1>HELLO WORLD</h1>
+        <div className='PanelOne'>
+            <div className='PanelOneContainer'>
+                <div className='PanelOneTextContainer'>
+                    <div className='Title' id='PanelOneTitle'>
+                        HELLO WORLD!
+                    </div>
+                    <div className='Subtext' id='PanelOneSubtext'>
+                        My name is Gabriel and I am a 3rd year computer science student in the pursuit of continuous learning. 
+                        I have been adopting and embracing the student mindset of seeking knowledge and growth. 
+                        I am experienced in Java, C, Python, and Javascript, and look forward to mastering them as well as gaining new skills along the way. 
+                        I'm always striving for creativity and logical thinking in everyday life.
+                    </div>
+                </div>
+                <div className='PanelOneImageContainer'>
+                    <img src={Gabe} alt='' className='PanelOneImage'/>
+                </div>
+            </div>
+            <div className='PanelOneDivider'/>
         </div>
     );
 }
+
+const PanelTwo = () => {
+    const [quotes, setQuotes] = useState([]);
+    const [current, setCurrent] = useState(0);
+    const [fadeIn, setFadeIn] = useState(true);
+   
+    useEffect(() => {
+        const fetchQuotes = async () => {
+            try {
+                const response = await fetch('/api/quotes');
+                const data = await response.json();
+                console.log(data[0].q);
+                setQuotes(data);
+            }
+            catch (error) {
+                console.error('error fetching quotes:', error);
+            }
+        };
+
+        fetchQuotes();
+    }, []);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFadeIn(false);
+            setTimeout(() => {
+                setCurrent((prevIndex) => (prevIndex + 1) % quotes.length);
+                setFadeIn(true);
+            }, 500); //fade out animation
+        }, 5000); //changes the quote with duration every 5 seconds
+
+        return () => clearInterval(interval);
+    }, [quotes]);
+   
+    return(
+        <div className='PanelTwo'>
+            {quotes.length > 0 && (
+                    <div className={`quote-container ${fadeIn ? 'fade-in' : ''}`}>
+                        <div className='quote'>{quotes[current].q}</div>
+                        <div className='author'>- {quotes[current].a}</div>
+                    </div>
+            )}
+        </div>
+    );
+}
+
 
 export default Home;
