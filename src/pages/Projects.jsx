ed72e87cb7bd21ea4070407c222d342ff52e3613
ghernan8 <ghernan8@uwo.ca>
2024-02-16 00:01:17 -0500

    adjusted the projectbox to column direction and fixed margin bottom

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -42,6 +42,20 @@ const PanelTwo = () => {
                 </div>
             </div>
 
+            <div className='ProjectBox'>
+                <div className='Subtext'>
+                    <b>project title here</b>
+                </div>
+                <div className='Subtext'>
+                    <ul>
+                        <li>Simulated real life work environment, tasked to debug and implement functionalities for CEO and stakeholders.</li>
+                        <li>Created accessible tools such as text to speech, speech to text, and dictation functionalities, aiding those with impairments.</li>
+                        <li>Developed website using JavaScript, html, CSS, NodeJS, ExpressJS, MySQL, and google cloud APIs.</li>
+                    </ul>
+                </div>
+            </div>
+            
+
 
 
         </div>
