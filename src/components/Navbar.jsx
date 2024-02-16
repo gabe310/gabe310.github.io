09661fd8a8cdfecb7f54e3d8d0fb4b28fc934307
ghernan8 <ghernan8@uwo.ca>
2024-02-16 17:41:40 -0500

    added scroll to top feature when you click to switch pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,13 @@ const Navbar = () => {
     const openPDF = () => {
         window.open(resumeFile, '_blank');
     };
+ 
+    const scrollToTop = () => { //to make it scroll to top after clicking a link
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth' //smooth scrolling animation
+        });
+      };
 
     return(
         <div className='Navbar'>
@@ -15,8 +22,8 @@ const Navbar = () => {
                 <h1>Gabriel Hernandez's Website</h1>
             </div>
                 <div className='NavbarSection'>
-                    <NavLink to='/'>About me</NavLink>
-                    <NavLink to='/projects'>Projects</NavLink>
+                    <NavLink to='/' onClick={scrollToTop}>About me</NavLink>
+                    <NavLink to='/projects' onClick={scrollToTop}>Projects</NavLink>
                     <a onClick={openPDF}>Resume</a>
                 </div>
         </div>
